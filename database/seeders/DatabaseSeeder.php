82bcfe9a6cd29603d820d292467a71dbc718ad7f
fadelchb <fadelchb@gmail.com>
2025-11-26 17:15:57 +0100

    merge

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
+        Team::factory(10)->create();
+        Stat::factory(10)->create();
+
 
         User::factory()->create([
             'name' => 'Test User',
