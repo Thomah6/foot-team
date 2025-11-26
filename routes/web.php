a3c4e11e18687aefe4a5a0da7e2381a4a1ca9c4d
Francis Frymce HOUNKPEVI <francisfrymcehounpkevi@gmail.com>
2025-11-26 22:01:53 +0100

    feat: crud team affectation a moitié la sauvegarde ne marche pas encore

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TeamController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -23,5 +24,24 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//Routes pour la creation de team et mercato par l'admin
+// Route::middleware(['auth', 'admin'])->group(function () {
+//     Route::resource('teams', TeamController::class);
+
+//     Route::post('/teams/assign-members', [TeamController::class, 'assignMembers']);
+//     Route::post('/teams/mercato', [TeamController::class, 'mercato']);
+// });
+ Route::get('/teams', [TeamController::class, 'vue'])->name('admin.teams');
+ Route::get('/teams/index', [TeamController::class, 'index'])->name('admin.teams.index');
+   Route::get('teams/create', [TeamController::class, 'create'])->name('admin.teams.create');
+    Route::post('/teams', [TeamController::class, 'store'])->name('admin.teams.store');
+    Route::get('/teams/{id}/edit', [TeamController::class, 'edit'])->name('admin.teams.edit');
+    Route::put('/teams/{team}', [TeamController::class, 'update'])->name('admin.teams.update');
+    Route::delete('/teams/{id}', [TeamController::class, 'destroy'])->name('admin.teams.destroy');
+Route::get('/teams/{team}/affect', [TeamController::class, 'affectPage'])
+    ->name('teams.affect');
+Route::post('/teams/{team}/affect/save', [TeamController::class, 'saveAffect']);
+
 
 require __DIR__.'/auth.php';
